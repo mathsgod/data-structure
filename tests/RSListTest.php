1b8d072f5de4fdb12557f5aad1cf8819e7751836
Raymond <mathsgod@yahoo.com>
2020-03-10 17:17:40 +0800

    update

--- tests/RSListTest.php
+++ tests/RSListTest.php
@@ -1,9 +1,15 @@
 <?
-declare (strict_types = 1);
+
+declare(strict_types=1);
 
 use PHPUnit\Framework\TestCase;
+use R\RSList;
 
 final class RSListTest extends TestCase
 {
-
+    public function testCreate()
+    {
+        $set = new RSList();
+        $this->assertInstanceOf(RSList::class, $set);
+    }
 }
